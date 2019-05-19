5bdecfdeb8260c4588a313f3e05aba83e793ca81
Cuong Pham <minhcuong2910@gmail.com>
2019-05-19 23:41:39 +0700

    Update design for menu left

--- app/src/main/java/com/ludwig/presentation/home/MainActivity.kt
+++ app/src/main/java/com/ludwig/presentation/home/MainActivity.kt
@@ -2,6 +2,8 @@ package com.ludwig.presentation.home
 
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
 import androidx.core.view.GravityCompat
 import com.ludwig.R
 import com.ludwig.presentation.base.BaseActivity
@@ -17,14 +19,6 @@ class MainActivity : BaseActivity() {
         replaceFragment(R.id.fragment_content, HomeFragment.newInstance())
     }
 
-    private fun initUI() {
-        setSupportActionBar(toolbar)
-        supportActionBar?.apply {
-            setHomeAsUpIndicator(R.drawable.ic_avatar_b)
-            setDisplayHomeAsUpEnabled(true)
-        }
-    }
-
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             android.R.id.home -> {
@@ -33,5 +27,29 @@ class MainActivity : BaseActivity() {
             }
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    private fun initUI() {
+        setSupportActionBar(toolbar)
+        supportActionBar?.apply {
+            setHomeAsUpIndicator(R.drawable.ic_avatar_b)
+            setDisplayHomeAsUpEnabled(true)
+        }
+
+        nav_drawer.getHeaderView(0).findViewById<View>(R.id.layout_profile).setOnClickListener(onDrawerMenuItemClicked)
+        nav_drawer.getHeaderView(0).findViewById<View>(R.id.tv_home).setOnClickListener(onDrawerMenuItemClicked)
+        nav_drawer.getHeaderView(0).findViewById<View>(R.id.tv_premium).setOnClickListener(onDrawerMenuItemClicked)
+        nav_drawer.getHeaderView(0).findViewById<View>(R.id.tv_history).setOnClickListener(onDrawerMenuItemClicked)
+        nav_drawer.getHeaderView(0).findViewById<View>(R.id.tv_contact).setOnClickListener(onDrawerMenuItemClicked)
+        nav_drawer.getHeaderView(0).findViewById<View>(R.id.tv_business).setOnClickListener(onDrawerMenuItemClicked)
+        nav_drawer.getHeaderView(0).findViewById<View>(R.id.tv_about).setOnClickListener(onDrawerMenuItemClicked)
+        nav_drawer.getHeaderView(0).findViewById<View>(R.id.tv_how_to_use).setOnClickListener(onDrawerMenuItemClicked)
+        nav_drawer.getHeaderView(0).findViewById<View>(R.id.tv_blog).setOnClickListener(onDrawerMenuItemClicked)
+        nav_drawer.getHeaderView(0).findViewById<View>(R.id.tv_press_kit).setOnClickListener(onDrawerMenuItemClicked)
+    }
+
+    private val onDrawerMenuItemClicked = View.OnClickListener {
+        drawer_layout.closeDrawer(GravityCompat.START)
+        Toast.makeText(this, "This feature is in development...", Toast.LENGTH_SHORT).show()
     }
 }
